0c6b10da45a56382a90b2f2b32ab1fddb8491aff
Thomas Kinsella <thomasjkinsella4@gmail.com>
2022-02-25 10:49:06 -0500

    commit tree cbd1737a08a6cff7a4f34337492cba7ba0020c4c

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // code your solution here
+
+//function superbowlWin(array) {
+//    array.find((didWin) => {
+//        if (didWin.result === "W") {
+//            console.log(didWin.year);
+//        } else {
+//            return (undefined);
+//        }
+//    })
+//}
+
+//superbowlWin(record);
+
+
+//function superbowlWin(recordsArray) {
+//    recordsArray.find((record) => {
+ //       if (win.result === "W") {
+//            return win.year
+//        } else {
+//            return undefined;
+//        }
+ //   })
+//};
+
+//superbowlWin(record);
+
+
+//myArray.find((element) => {
+//    console.log(element > 3);
+//});
+
+function superbowlWin(recordsArray) {
+    const foundRecord =  recordsArray.find(record => record.result === "W");
+    if (foundRecord) {
+        return foundRecord.year;
+    } else {
+        return undefined;
+    }
+}
+
